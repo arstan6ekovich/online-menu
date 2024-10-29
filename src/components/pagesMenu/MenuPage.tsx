f8aa39789014b37fb0ffa634372315db686b8648
Marlen <zomadujsonov@gmail.com>
2024-10-29 11:58:47 +0600

    menupage

--- src/components/pagesMenu/MenuPage.tsx
+++ src/components/pagesMenu/MenuPage.tsx
@@ -1,23 +1,36 @@
 "use client";
 import { useMemo, useState } from "react";
+import Image from "next/image";
 import scss from "./MenuPage.module.scss";
 import { products } from "@/Products";
-import { useLanguageStore } from "@/stores/zustand"; 
+import { useLanguageStore } from "@/stores/zustand";
 
 const Menu = () => {
-  const { t } = useLanguageStore(); 
+  const { t } = useLanguageStore();
   const [activeItem, setActiveItem] = useState("desserts-001");
-  const [loading, setLoading] = useState(false);
   const items = [
     { id: "desserts-001", title: t("Десерты", "Desserts", "Десерттер") },
-    { id: "hot-drinks-002", title: t("Горячие напитки", "Hot Drinks", "Ысык ичимдиктер") },
-    { id: "cold-drinks-003", title: t("Холодные напитки", "Cold Drinks", "Суук ичимдиктер") },
-    { id: "national-foods-004", title: t("Национальные блюда", "National Foods", "Улуттук тамактар") },
-    { id: "eastern-cuisine-005", title: t("Восточная кухня", "Eastern Cuisine", "Чыгыш ашканасы") },
+    {
+      id: "hot-drinks-002",
+      title: t("Горячие напитки", "Hot Drinks", "Ысык ичимдиктер"),
+    },
+    {
+      id: "cold-drinks-003",
+      title: t("Холодные напитки", "Cold Drinks", "Суук ичимдиктер"),
+    },
+    {
+      id: "national-foods-004",
+      title: t("Национальные блюда", "National Foods", "Улуттук тамактар"),
+    },
+    {
+      id: "eastern-cuisine-005",
+      title: t("Восточная кухня", "Eastern Cuisine", "Чыгыш ашканасы"),
+    },
     { id: "fast-foods-006", title: t("Фаст-фуды", "Fast Foods", "Фаст-фуд") },
   ];
 
   const [activeProduct, setActiveProduct] = useState<null | string>(null);
+
   function handleClick(item: string) {
     setActiveProduct(item);
     setActiveItem(item);
@@ -25,12 +38,12 @@ const Menu = () => {
 
   const product = useMemo(
     () => products.find((el) => el.id === activeProduct),
-    [products, activeProduct]
+    [activeProduct]
   );
 
   const filterProducts = useMemo(
     () => products.filter((el) => el.categoryId === activeItem),
-    [products, activeItem]
+    [activeItem]
   );
 
   const scrollToTop = () => {
@@ -39,14 +52,14 @@ const Menu = () => {
       behavior: "smooth",
     });
   };
-  
+
   return (
     <div id={scss.menuPage}>
       <div className="container">
         {activeProduct && product && (
           <button
             className={scss.closeBtn}
-            onClick={() => setActiveProduct("")}
+            onClick={() => setActiveProduct(null)}
           >
             X
           </button>
@@ -71,9 +84,11 @@ const Menu = () => {
                 <div className={scss.productContent}>
                   <div key={product.id} className={scss.productCardTwo}>
                     <img
-                      src={product.imageSrc}
+                      src=""
                       alt={product.name}
                       className={scss.productImage}
+                      width={300} // Укажите подходящие значения для Image
+                      height={300}
                     />
                     <div className={scss.produtTextTwo}>
                       <div className={scss.iceCreamTwo}>
@@ -86,28 +101,16 @@ const Menu = () => {
                   <div className={scss.extras}>
                     <h3>{t("Дополнения", "Extras", "Кошумча")}</h3>
                     <hr />
-
-                    <div className={scss.charyBlock}>
-                      <p>{t("Вишня", "Cherry", "Чоң көйкөс")}</p>
-                      <p>$0.90</p>
-                    </div>
                     <div className={scss.charyBlock}>
                       <p>{t("Вишня", "Cherry", "Чоң көйкөс")}</p>
                       <p>$0.90</p>
                     </div>
                     <hr />
-                    <h3>{t("Дополнения", "Extras", "Кошумча")}</h3>
-                    <div className={scss.charyBlock}>
-                      <p>{t("Вишня", "Cherry", "Чоң көйкөс")}</p>
-                      <p>$0.90</p>
-                    </div>
-                    <div className={scss.charyBlock}>
-                      <p>{t("Вишня", "Cherry", "Чоң көйкөс")}</p>
-                      <p>$0.90</p>
-                    </div>
                   </div>
                 </div>
-                  <p className={scss.P}>{t("Похожие товары", "Similar products", "Ушундай товарлар")}</p>
+                <p className={scss.P}>
+                  {t("Похожие товары", "Similar products", "Ушундай товарлар")}
+                </p>
               </div>
             )}
             <div className={scss.productList}>
@@ -117,12 +120,13 @@ const Menu = () => {
                   key={el.id}
                   className={scss.productCard}
                 >
-                  <img
-                    onLoad={() => setLoading(true)}
+                  <Image
                     onClick={scrollToTop}
                     src={el.imageSrc}
                     alt={el.name}
                     className={scss.productImage}
+                    width={300}
+                    height={300}
                   />
                   <div className={scss.produtText}>
                     <div className={scss.iceCream}>
