b005aaa2e1281a59bd4bad30231926785569fa52
user <ularbekkochkorov0@gmail.com>
2024-10-29 11:25:56 +0600

    comm2

--- src/components/pages/visit/VisitPage.tsx
+++ src/components/pages/visit/VisitPage.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import scss from "./VisitPage.module.scss";
 import Image from "next/image";
@@ -60,9 +60,11 @@ const Visit = () => {
             </div>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d477.16048450867737!2d74.61237025479795!3d42.889812057316234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1729328939461!5m2!1sru!2skg"
-              width="550px"
+              width="100%" 
               height="250"
               loading="lazy"
+              style={{ border: 0, maxWidth: "100%" }} 
+              allowFullScreen
             ></iframe>
           </div>
         </div>
